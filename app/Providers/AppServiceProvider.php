b5deeae94ac493f0f4dfb3942cbdfbdca0edd9cb
anhnt81 <anhnt30194@gmail.com>
2017-10-07 22:15:24 +0700

    add route get post

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Category;
+use App\Brand;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +17,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        //share view data category
+        view()->composer('front-end.layouts.header',function($view){
+            $category = Category::with('childHas')->where('parentId',null)->get();
+            $brand = Brand::all();
+            $view->with('category',$category);
+        });
+        //share view data brand
+        view()->composer('front-end.layouts.header',function($view){
+            $brand = Brand::all();
+            $view->with('brand',$brand);
+        });
     }
 
     /**
